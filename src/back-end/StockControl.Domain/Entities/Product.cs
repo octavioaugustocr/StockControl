1205b6c40306c025e608d9995b80b6ce8286dd8e
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-07 20:59:07 -0300

    feat: adding product entity

---
+++ src/back-end/StockControl.Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace StockControl.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Flavor { get; set; } = string.Empty;
+        public string Mark { get; set; } = string.Empty;
+        public string Manufacturer { get; set; } = string.Empty;
+        public DateOnly DueDate { get; set; }
+        public int QuantityStock { get; set; }
+    }
+}
