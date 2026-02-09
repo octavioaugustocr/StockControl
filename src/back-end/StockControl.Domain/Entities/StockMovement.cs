33c36d952eedd42f77282ffc22c00608f071a6bc
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-09 20:44:02 -0300

    feat: adding stock movement entity

---
+++ src/back-end/StockControl.Domain/Entities/StockMovement.cs
@@ -0,0 +1,11 @@
+﻿using StockControl.Domain.Enums;
+
+namespace StockControl.Domain.Entities
+{
+    public class StockMovement : EntityBase
+    {
+        public int Quantity { get; set; }
+        public TypeMovement TypeMovement { get; set; }
+        public long ProductId { get; set; }
+    }
+}
