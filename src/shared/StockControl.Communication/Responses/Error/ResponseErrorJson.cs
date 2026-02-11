fbaf4833aa440fd76d647e70ea537c235b78af47
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-10 21:35:39 -0300

    feat: adding response error json

---
+++ src/shared/StockControl.Communication/Responses/Error/ResponseErrorJson.cs
@@ -0,0 +1,17 @@
+﻿namespace StockControl.Communication.Responses.Error
+{
+    public class ResponseErrorJson
+    {
+        public IList<string> Errors { get; set; }
+
+        public ResponseErrorJson(IList<string> errors) => Errors = errors; 
+
+        public ResponseErrorJson(string error)
+        {
+            Errors = new List<string>
+            {
+                error
+            };
+        }
+    }
+}
