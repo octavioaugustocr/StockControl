ea0f86b0f33c6ece1a1c43355dcffe508571bceb
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-11 15:55:44 -0300

    feat: adding stock control exception

---
+++ src/shared/StockControl.Exceptions/ExceptionsBase/StockControlException.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace StockControl.Exceptions.ExceptionsBase
+{
+    public abstract class StockControlException : SystemException
+    {
+        public StockControlException(string message) : base(message) { }
+
+        public abstract IList<string> GetErrorMessages();
+        public abstract HttpStatusCode GetStatusCode();
+    }
+}
