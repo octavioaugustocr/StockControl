16dbbed2f0acb903ceca4f52a358bd2b42c9d07f
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-13 21:54:30 -0300

    feat: adding not found exception

---
+++ src/shared/StockControl.Exceptions/ExceptionsBase/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace StockControl.Exceptions.ExceptionsBase
+{
+    public class NotFoundException : StockControlException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public override IList<string> GetErrorMessages() => [Message];
+
+        public override HttpStatusCode GetStatusCode() => HttpStatusCode.NotFound;
+    }
+}
