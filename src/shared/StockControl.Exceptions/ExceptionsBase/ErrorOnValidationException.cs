27ae8c9ce1c5869f2856526c76eed0bb78994b19
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-16 23:18:36 -0300

    feat: adding error on validation exception

---
+++ src/shared/StockControl.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace StockControl.Exceptions.ExceptionsBase
+{
+    public class ErrorOnValidationException : StockControlException
+    {
+        public IList<string> _errorMessages { get; set; }
+
+        public ErrorOnValidationException(IList<string> errorMessages) : base(string.Empty) => _errorMessages = errorMessages;
+
+        public override IList<string> GetErrorMessages() => _errorMessages;
+
+        public override HttpStatusCode GetStatusCode() => HttpStatusCode.BadRequest;
+    }
+}
