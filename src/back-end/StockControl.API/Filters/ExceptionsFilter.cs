9cde391bd2375bc102d3f9a1ae625052ae0a7aaa
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-17 23:15:42 -0300

    feat: adding exception filter

---
+++ src/back-end/StockControl.API/Filters/ExceptionsFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using StockControl.Communication.Responses.Error;
+using StockControl.Exceptions;
+using StockControl.Exceptions.ExceptionsBase;
+using System.Net;
+
+namespace StockControl.API.Filters
+{
+    public class ExceptionsFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is StockControlException stockControlException)
+                HandleProjectException(stockControlException, context);
+            else
+                ThrowUnknownErrorException(context);
+        }
+
+        private void HandleProjectException(StockControlException stockControlException, ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = (int)stockControlException.GetStatusCode();
+            context.Result = new ObjectResult(new ResponseErrorJson(stockControlException.GetErrorMessages()));
+        }
+
+        private void ThrowUnknownErrorException(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessagesExceptions.UNKNOWN_ERROR));
+        }
+    }
+}
