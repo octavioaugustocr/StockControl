8d567e75a8008d40d0723312bb51a49aa521e4ec
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-21 21:51:44 -0300

    feat: adding db context

---
+++ src/back-end/StockControl.Infrastructure/DataAccess/StockControlDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockControl.Domain.Entities;
+
+namespace StockControl.Infrastructure.DataAccess
+{
+    public class StockControlDbContext : DbContext
+    {
+        public StockControlDbContext(DbContextOptions<StockControlDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<StockMovement> StockMovements { get; set; }
+    }
+}
