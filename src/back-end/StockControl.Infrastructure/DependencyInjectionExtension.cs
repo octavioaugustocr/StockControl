658d0f89502a7690822bd845a369a675cc7ba5da
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-23 20:52:41 -0300

    feat: adding dependency injection extension in infrastructure

---
+++ src/back-end/StockControl.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StockControl.Infrastructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
